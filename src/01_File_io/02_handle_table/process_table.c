ebd51c8e037a9bc65c8ca1b368a9c3b87aba3a9e
Kline <Kline@example.com>
2024-03-02 18:36:42 +0800

    debug push to githbub

--- src/01_File_io/02_handle_table/process_table.c
+++ src/01_File_io/02_handle_table/process_table.c
@@ -156,6 +156,7 @@ int student_info_handler(const char* info, char * result, int len)
             temp = strtok(NULL, ",");
             i++;
         }
+        /* continue to... */
 
         if (i == 1) {
             LOG_DEBUG("Too few parameters in this line: %s", line);
