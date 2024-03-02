d3ce7ea6b3e2878c0a8d60db3c0bee5de4a1584f
KlineSun <125962701+KlineSun@users.noreply.github.com>
2024-03-02 19:50:21 +0800

    Add files via upload

--- src/01_File_io/02_handle_table/process_table.c
+++ src/01_File_io/02_handle_table/process_table.c
@@ -156,6 +156,9 @@ int student_info_handler(const char* info, char * result, int len)
             temp = strtok(NULL, ",");
             i++;
         }
+        /* continue to... */
+        /* continue to... */
+        /* continue to... */
 
         if (i == 1) {
             LOG_DEBUG("Too few parameters in this line: %s", line);
