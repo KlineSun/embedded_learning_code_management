b51b2443a515626be0007641435f5aeccaa194c1
Kline <Kline@example.com>
2024-04-04 17:38:35 +0800

    解析学生数据成功，提交备份

--- src/01_File_io/02_handle_table/process_table.c
+++ src/01_File_io/02_handle_table/process_table.c
@@ -21,10 +21,64 @@ char *get_time_str() {
     return time_str;
 }
 
-static int g_info_type = -1;
-
-static student_t *g_student_ptr = NULL;
+static char g_table_type[64] = {0};
+static int g_rows = 0;
+static int g_columns = 0;
+static student_t *g_student_info = NULL;
 static fruit_t *g_fruit_ptr = NULL;
+
+
+/**
+ * @brief: Read the contents of the entire file
+ * 
+ * @param path: file path
+ * 
+ * return: Return buf that save the contents of the file on sucess. The buf must gree by caller!
+ *         Returns a null pointer if it fails!
+ * 
+*/
+char *common_read_all_file_contents(const char *path)
+{
+    if (NULL == path) {
+        LOG_DEBUG("invalid parameter!");
+        return NULL;
+    }
+
+    FILE *fp = fopen(path, "r");
+    if (NULL == fp) {
+        LOG_DEBUG("open parameter!");
+        return NULL;
+    }
+
+    // get file size
+    fseek(fp, 0, SEEK_END);
+    long fileSize = ftell(fp);
+    LOG_DEBUG("The size of %s is: %ld", path, fileSize);
+
+    // malloc buf
+    char *buf = (char *)malloc(sizeof(char) * (fileSize + 1));
+    if (NULL == buf) {
+        LOG_DEBUG("malloc buffer failed!");
+        fclose(fp);
+        return NULL;
+    }
+    memset(buf, 0, (fileSize + 1));
+
+    // get file content
+    rewind(fp);
+    long read_size = fread(buf, 1, fileSize, fp);
+    if (read_size != fileSize) {
+        LOG_DEBUG("malloc buffer failed!");
+        free(buf);
+        fclose(fp);
+        return NULL;
+    }
+    buf[fileSize] = '\0';
+
+    //LOG_DEBUG("File: %s, contents:\n %s", path, buf);
+    fclose(fp);
+    return buf;
+}
 
 /**
  * 
@@ -51,6 +105,8 @@ int read_line(int fd, char *buf, int max_len)
             break;
         }
         if (ch == '\n' || ch == '\r' || ch == '\0') {
+            // 每当遇到换行、结束符等特殊字符后，文件index向后偏移一个字符
+            lseek(fd, 1, SEEK_CUR);
             break;
         }
         if (i < max_len - 1) {
@@ -73,129 +129,415 @@ int read_line(int fd, char *buf, int max_len)
     return -1;
 }
 
-int parse_type(const char* buf)
+int parse_table_type(const char* table_path)
 {
-    if (buf == NULL) {
+    if (table_path == NULL) {
        LOG_DEBUG("invalid parameter!");
        return -1;
     }
 
-    char *temp = strstr(buf, "info_type");
-    if (temp == NULL)
+    char *file_buf = common_read_all_file_contents(table_path);
+    if (NULL == file_buf) {
+        LOG_DEBUG("read %s failed!", table_path);
         return -1;
+    }
+
+    char *temp = strstr(file_buf, "table_type");
+    if (NULL == temp) {
+        LOG_DEBUG("No type defined in the table");
+        free(file_buf);
+        return -1;
+    }
 
     char *type = strtok(temp, ",");
     type = strtok(NULL, ",");
-    if (type == NULL)
+    if (NULL == type || strlen(type) < 1) {
+        LOG_DEBUG("No type defined in the table");
+        free(file_buf);
         return -1;
-    
-    if (0 == strncasecmp(type, "student")) {
-        return STUDENT;
-    } else if (0 == strncasecmp(type, "fruit")) {
-        return FRUIT;
+    }
+
+    LOG_DEBUG("get type: %s", type);
+    if (0 == strcasecmp(type, "course_table")) {
+        strncpy(g_table_type, "course_table", sizeof(g_table_type));
+    } else if (0 == strcasecmp(type, "sale_table")) {
+        strncpy(g_table_type, "sale_table", sizeof(g_table_type));
     } else {
         LOG_DEBUG("Unsupport info type!");
+        free(file_buf);
         return -1;
     }
+
+    free(file_buf);
+    return 0;
 }
 
-
-int student_info_handler(const char* info, char * result, int len)
+int count_sub_str(const char* str, const char *sub_str)
 {
-    if (info == NULL || result == NULL || len < 0) {
+    if (NULL == str || NULL == sub_str) {
         LOG_DEBUG("invalid parameter!");
-        return -1;
-    }
-
-    if (strstr(line, "name") == NULL && g_student_ptr == NULL) {
-        LOG_DEBUG("Configuration fields not read!");
-        return -1;
-    }
-
-    // parse config field
-    if (g_student_ptr == NULL) {
-        g_student_ptr = malloc(sizeof(student_t));
-        if (g_student_ptr == NULL) {
-            LOG_DEBUG("Malloc error!");
-            return -1;
-        }
-        int i = 1;
-        char *temp = NULL;
-        temp = strtok(info, ",");
-        temp = strtok(NULL, ",");
-        /*  CHINESE = 1,
-            MATH,
-            ENGILISH,
-            PHYSICS,
-            CHEMISTRY,
-            BIOLOGY,
-            POLITICS,
-            HISTORY,
-            GEOGRAPHY
-        */
-        while (temp != NULL) {
-            if (strncasecmp(temp, "Total") == 0) {
-
-            } else if (strncasecmp(temp, "Rank") == 0) {
-
-            } else if (strncasecmp(temp, "Mark") == 0) {
-
-            } else if (strncasecmp(temp, "Chinese") == 0) {
-            } else if (strncasecmp(temp, "Math") == 0) {
-            } else if (strncasecmp(temp, "English") == 0) {
-            } else if (strncasecmp(temp, "Physics") == 0) {
-            } else if (strncasecmp(temp, "Chemistry") == 0) {
-            } else if (strncasecmp(temp, "Biology") == 0) {
-            } else if (strncasecmp(temp, "Politics") == 0) {
-            } else if (strncasecmp(temp, "History") == 0) {
-            } else if (strncasecmp(temp, "Geography") == 0) {
-            } else {
-                LOG_DEBUG("Unidentified information!");
-                continue;
-            }
-            temp = strtok(NULL, ",");
-            i++;
-        }
-
-        if (i == 1) {
-            LOG_DEBUG("Too few parameters in this line: %s", line);
-            return -1;
-        }
-    } else {
-        // 使用链表保存
-    }
-}
-
-int parse_line(const char *line, char * result, int len)
-{
-    if (line == NULL || result == NULL || len <= 0) {
-        LOG_DEBUG("invalid parameter!");
-        return -1;
-    }
-
-    // parse type
-    if (g_info_type < 0) {
-        int t = parse_type(line);
-        if (t < 0) {
-            LOG_DEBUG("Parse information type failed!");
-            return -1;
-        }
-        LOG_DEBUG("Get information type: %d", t);
-        g_info_type = t;
         return 0;
     }
 
-    int i = 0;
-    for (; i < LIST_LENGTH(g_info_handle_list); i++) {
-        if (g_info_handle_list[i].type == g_info_type) {
-            int rc = g_info_handle_list[i].info_handle_func(line, result, len);
-            return rc;
-        }
+    int cnt = 0;
+    char *buf, *p;
+    buf = (char *)malloc(sizeof(char) * (strlen(str) + 1));
+    if (NULL == buf) {
+        LOG_DEBUG("malloc error!");
+        return 0;
     }
+    strcpy(buf, str);
+    buf[strlen(str)] = '\0';
+
+    p = buf;
+    while (NULL != (p = strstr(p, sub_str))){
+        cnt++;
+        p++;
+    }
+
+    //LOG_DEBUG("Found %d \"%s\" in  \"%s\"", cnt, sub_str, buf);
+    free(buf);
+    return cnt;
+}
+
+int parse_csv_line(const char *line, char ***result_list)
+{
+    if (NULL == line) {
+        LOG_DEBUG("invalid parameter!");
+        return -1;
+    }
+
+    int param_cnt = 0;
+    char **items_list = NULL;
+    param_cnt = count_sub_str(line, ",");
+    if (0 < param_cnt) {
+        // 为二维字符数组创建 n + 1个指针
+        items_list = (char **)malloc((param_cnt + 1) * sizeof(char *));
+        if (NULL == items_list) {
+            LOG_DEBUG("malloc error!");
+            return -1;
+        }
+    } else {
+        LOG_DEBUG("invalid parameter!");
+        return -1;
+    }
+
+    //拷贝line字符串
+    char *line_buf;
+    line_buf = (char *)malloc(sizeof(char) * (strlen(line) + 1));
+    if (NULL == line_buf) {
+        LOG_DEBUG("malloc error!");
+        return -1;
+    }
+    strcpy(line_buf, line);
+    line_buf[strlen(line)] = '\0';
+
+    //提取字符串中的item
+    char temp_buf[MAX_BUF_SIZE] = {0};
+    char ch = '\0';
+    char *ptr = line_buf;
+    int i = 0, j = 0, sub_str_size = 0;
+    for (j = 0; j < strlen(line) + 1; j++, ptr++) {
+        ch = ptr[0];
+        //LOG_DEBUG("ch: %s", &ch);
+        if (',' != ch && '\0' != ch) {
+            strncat(temp_buf, &ch, 1);
+            sub_str_size++;
+            if (sub_str_size >= MAX_BUF_SIZE) {
+                LOG_DEBUG("content is too long!");
+                free(line_buf);
+                return -1;
+            }
+            continue;
+        }
+
+        items_list[i] = (char *)malloc(sub_str_size + 1);
+        if (NULL == items_list[i]) {
+            LOG_DEBUG("malloc items_list[%d] error, skip to next!", i);
+            sub_str_size = 0;
+            continue;
+        }
+        
+        memset(items_list[i], 0, sub_str_size + 1);
+        if (sub_str_size > 0) {
+            strcpy(items_list[i], temp_buf);
+        } else {
+            strcpy(items_list[i], "");
+        }
+        items_list[i][sub_str_size] = '\0';
+        //LOG_DEBUG("column[%d]: %s", i, items_list[i]);
+        i++;
+        if ('\0' == ch) {
+            break;
+        }
+        sub_str_size = 0;
+        memset(temp_buf, 0, sizeof(temp_buf));
+    }
+
+    g_columns = i;
+    *result_list = items_list;
+    free(line_buf);
+    return 0;
+}
+
+void free_2d_array(char **array_ptr)
+{
+    if (NULL == array_ptr) {
+        LOG_DEBUG("array_ptr is NULL, no need to free");
+        return;
+    }
+
+    int i;
+    for (i = 0; i < g_columns; i++) {
+        free(array_ptr[i]);
+        array_ptr[i] == NULL;
+    }
+    free(array_ptr);
+    array_ptr = NULL;
+    return;
+}
+
+
+int get_data_type_emum(char *str)
+{
+    if (NULL == str) {
+        LOG_DEBUG("invalid parameter!");
+        return INVALID_TYPE;
+    }
+
+    if (0 == strcmp(str, "Name")) {
+        return NAME;
+    } else if (0 == strcmp(str, "Total")) {
+        return TOTAL;
+    } else if (0 == strcmp(str, "Rank")) {
+        return RANK;
+    } else if (0 == strcmp(str, "Mark")) {
+        return MARK;
+    } else if (0 == strcmp(str, "Chinese")) {
+        return CHINESE;
+    } else if (0 == strcmp(str, "Math")) {
+        return MATH;
+    } else if (0 == strcmp(str, "English")) {
+        return ENGLISH;
+    } else if (0 == strcmp(str, "Physics")) {
+        return PHRSICS;
+    } else if (0 == strcmp(str, "Chemistry")) {
+        return CHEMMISTR;
+    } else if (0 == strcmp(str, "Biology")) {
+        return BIOLOGY;
+    } else if (0 == strcmp(str, "Politics")) {
+        return POLITICS;
+    } else if (0 == strcmp(str, "History")) {
+        return HISTORY;
+    } else if (0 == strcmp(str, "Geography")) {
+        return GEOGRAPHY;
+    } else {
+        LOG_DEBUG("unsupport type: %s", str);
+        return INVALID_TYPE;
+    }
+    return INVALID_TYPE;
+}
+
+int parse_student_info(char **items_list, char **data_list, student_t *student_info)
+{
+    if (NULL == items_list || NULL == data_list || NULL == student_info) {
+        LOG_DEBUG("invalid parameter!");
+        return -1;
+    }
+
+    int i = 0;
+    course_t *course_ptr;
+    student_info->course_count = 0;
+    student_info->total_score = 0;
+    student_info->rank = 1;
+    float score = 0.0;
+    for (; i < g_columns; i++) {
+        LOG_DEBUG("get student info: %s = %s", items_list[i], data_list[i]);
+
+        switch (get_data_type_emum(items_list[i]))
+        {
+            case NAME:
+            case TOTAL:
+            case RANK:
+            case MARK:
+                strcpy(student_info->name, data_list[i]);
+                break;
+            case CHINESE:
+            case MATH:
+            case ENGLISH:
+            case PHRSICS:
+            case CHEMMISTR:
+            case BIOLOGY:
+            case POLITICS:
+            case HISTORY:
+            case GEOGRAPHY:
+                // malloc next course
+                course_ptr = (course_t *)malloc(sizeof(course_t));
+                if (NULL == course_ptr) {
+                    LOG_DEBUG("malloc course struct failed!");
+                    return -1;
+                }
+
+                // set link
+                if (NULL == student_info->course) {
+                    student_info->course = course_ptr;
+                    student_info->next = NULL;
+                } else {
+                    course_ptr->next = student_info->course;
+                    student_info->course = course_ptr;
+                }
+
+                // set current course value to course struct
+                strcpy(course_ptr->name, items_list[i]);
+                score = atof(data_list[i]);
+                if (data_list[i][0] != '0' && score <= 0) {
+                    LOG_DEBUG("score abnormal: %s, get float value: %f", data_list[i], score);
+                    score = 0.0;
+                }
+                course_ptr->score = score;
+                student_info->total_score += course_ptr->score;
+                student_info->course_count++;
+                break;
+            default:
+                LOG_DEBUG("unsupport item: %s", items_list[i]);
+                break;
+        }
+        course_ptr = NULL;
+    }
+    LOG_DEBUG("The student scored total of %f points in %d courses", student_info->total_score, student_info->course_count);
 
     return 0;
 }
 
+int student_info_handler(const char *input_file, const char *output_file)
+{
+    if (NULL == input_file || NULL == output_file) {
+        LOG_DEBUG("invalid parameter!");
+        return -1;
+    }
+    LOG_DEBUG("Enter!");
+
+    //handle input file
+    int is_success = 0;
+    int raw_file_fd = -1, output_file_fd = -1;
+    char line_buf[1024] = {0};
+    int read_cnt = -1, ret = 0;
+    char **items_list = NULL;
+    char **data_list = NULL;
+    raw_file_fd = open(input_file, O_RDONLY);
+    if (raw_file_fd < 0) {
+        LOG_DEBUG("open error");
+        is_success = -1;
+        goto exit_deal;
+    }
+
+    output_file_fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (output_file_fd < 0) {
+        close(raw_file_fd);
+        LOG_DEBUG("open error");
+        is_success = -1;
+        goto exit_deal;
+    }
+
+    // 逐行解析
+    while (1)
+    {
+        LOG_DEBUG("=================================================");
+        read_cnt = read_line(raw_file_fd, line_buf, sizeof(line_buf));
+        if (read_cnt <= 0){
+            break;
+        }
+
+        LOG_DEBUG("line_buf is: %s", line_buf);
+        // 解析item行
+        if (NULL == items_list && 0 < count_sub_str(line_buf, "Name,") && 0 < count_sub_str(line_buf, "Total")) {
+            ret = parse_csv_line(line_buf, &items_list);
+            if (0 != ret) {
+                LOG_DEBUG("Parse items failed!");
+                is_success = -1;
+                goto exit_deal;
+            }
+            continue;
+        } else if (NULL != strstr(line_buf, "table_type")) {
+            LOG_DEBUG("skip line of table_type");
+            continue;
+        }
+        if (NULL == items_list) {
+            LOG_DEBUG("Parse items failed!");
+            is_success = -1;
+            goto exit_deal;
+        }
+
+        // 解析数据行
+        ret = parse_csv_line(line_buf, &data_list);
+        if (NULL == data_list) {
+            LOG_DEBUG("Parse data failed!");
+            is_success = -1;
+            goto exit_deal;
+        }
+        g_rows++;
+        LOG_DEBUG("For %d student, there are %d pieces of information", g_rows, g_columns);
+
+        // 初始化当前学生的结构体
+        student_t *tmp = (student_t *)malloc(sizeof(student_t));
+        if (NULL == tmp) {
+            LOG_DEBUG("malloc student struct failed!");
+            goto exit_deal;
+        }
+        if (NULL == g_student_info) {
+            g_student_info = tmp;
+        } else {
+            tmp->next = g_student_info;
+            g_student_info = tmp;
+        }
+        memset(g_student_info, 0, sizeof(student_t));
+
+        // 写入数据到结构体中
+        ret = parse_student_info(items_list, data_list, g_student_info);
+        if (0 != ret) {
+            LOG_DEBUG("parse student info failed!");
+            goto exit_deal;
+        }
+
+        memset(line_buf, 0, sizeof(line_buf));
+    }
+
+exit_deal:
+    free_2d_array(items_list);
+    free_2d_array(data_list);
+    if (raw_file_fd > 0) {
+        close(raw_file_fd);
+    }
+    if (output_file_fd > 0) {
+        close(output_file_fd);
+    }
+    while (NULL != g_student_info) {
+        while (NULL != g_student_info->course) {
+            LOG_DEBUG("Debug");
+            course_t *temp1 = g_student_info->course;
+            g_student_info->course = g_student_info->course->next;
+            free(temp1);
+        }
+        student_t *temp2 = g_student_info;
+        g_student_info = g_student_info->next;
+        free(temp2);
+    }
+    return is_success;
+}
+
+int fruit_info_handler(const char *input_file, const char *output_file)
+{
+    if (NULL == input_file || NULL == output_file) {
+        LOG_DEBUG("invalid parameter!");
+        return -1;
+    }
+    LOG_DEBUG("Enter!");
+
+    //handle input file
+
+
+    return 0;
+}
 
 /**
  * usage: ./process_table <raw csv file> -[operate type] <output csv file>
@@ -213,13 +555,16 @@ int main(int argc, char const *argv[])
     }
 
     char raw_file[128] = {0};
-    snprintf(raw_file, sizeof(raw_file), "%s", argv[1]);
+    strncpy(raw_file, argv[1], sizeof(raw_file));
+    //snprintf(raw_file, sizeof(raw_file), "%s", argv[1]);
 
     char operate_type[16] = {0};
     char output_file[128] = {0};
     if (argc == 4) {
-        snprintf(operate_type, sizeof(operate_type), "%s", argv[2]);
-        snprintf(output_file, sizeof(output_file), "%s", argv[3]);
+        strncpy(operate_type, argv[2], sizeof(operate_type));
+        strncpy(output_file, argv[3], sizeof(output_file));
+        //snprintf(operate_type, sizeof(operate_type), "%s", argv[2]);
+        //snprintf(output_file, sizeof(output_file), "%s", argv[3]);
     } else {
         LOG_DEBUG("Too many paramter!\r\nUsage: ./process_table <raw csv file> -[operate type] <output csv file>");
     }
@@ -236,37 +581,24 @@ int main(int argc, char const *argv[])
 
     LOG_DEBUG("raw_file = %s, operate type = %s, output file = %s", raw_file, operate_type, output_file);
 
-    int raw_file_fd = open(raw_file, O_RDONLY);
-    if (raw_file_fd < 0) {
-        perror("open error");
+    // parse the table type
+    int ret = parse_table_type(raw_file);
+    if (0 != ret || 0 == strcmp(g_table_type, "")) {
+        LOG_DEBUG("parse the type of table failed!");
         return EXIT_FAILURE;
     }
 
-    int output_file_fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (output_file_fd < 0) {
-        perror("open error");
-        return EXIT_FAILURE;
+    int i = 0;
+    for (i = 0; i < LIST_LENGTH(g_info_handle_list); i++) {
+        if (0 == strcmp(g_table_type, g_info_handle_list[i].type_name)) {
+            ret = g_info_handle_list[i].info_handle_func(raw_file, output_file);
+            if (0 != ret) {
+                LOG_DEBUG("handle table failed!");
+                return EXIT_FAILURE;
+            }
+        }
     }
 
-    LOG_DEBUG("open file success, raw_file_fd = %d, output_file_fd = %d", raw_file_fd, output_file_fd);
-
-    char line_buf[1024] = {0};
-    int read_cnt = -1;
-    while (1)
-    {
-        read_cnt = read_line(raw_file_fd, line_buf, sizeof(line_buf));
-        if (read_cnt <= 0)
-            break;
-        LOG_DEBUG("line_buf = %s", line_buf);
-
-        
-    }
-
-    if (g_student_ptr != NULL) {
-        free(g_student_ptr);
-        g_student_ptr = NULL;
-    }
-    close(raw_file_fd);
-    close(output_file_fd);
+    LOG_DEBUG("exit normal!");
     return 0;
 }
--- src/01_File_io/02_handle_table/process_table.h
+++ src/01_File_io/02_handle_table/process_table.h
@@ -2,100 +2,56 @@
 
 #define MAX_BUF_SIZE 2048
 #define LIST_LENGTH(x) (sizeof(x) / sizeof((x)[0]))
+#define null (NULL)
+#define true (TRUE)
+#define false (FALSE)
 
 char *get_time_str();
 #define LOG_DEBUG(format, ...) printf("%s %s() %d: "format"\n", get_time_str(), __func__, __LINE__, ##__VA_ARGS__)
 
-typedef enum {
-    STUDENT = 0,
-    FRUIT
-} table_info_type;
+int student_info_handler(const char *input_file, const char *output_file);
+int fruit_info_handler(const char *input_file, const char *output_file);
 
 typedef struct
 {
-    table_info_type type;
-    int (*info_handle_func)(const char *info, char *result, int len);
+    char *type_name;
+    int (*info_handle_func)(const char *input_file, const char *output_file);
 } info_handle_t;
 
-/*typedef struct {
-    // table info type
-    table_info_type type;
-
-    // parse type of info
-    int (*parse_type)(char *info);
-
-    // handle input information
-    info_handle_t *info_handle;
-} table_parser_t;*/
-
+static info_handle_t g_info_handle_list[] = {
+    {"course_table", student_info_handler},
+    {"sale_table",   fruit_info_handler}
+};
 
 // student type struct begin
 typedef enum {
-    CHINESE = 1,
+    NAME = 0,
+    CHINESE,
     MATH,
-    ENGILISH,
-    PHYSICS,
-    CHEMISTRY,
+    ENGLISH,
+    PHRSICS,
+    CHEMMISTR,
     BIOLOGY,
     POLITICS,
     HISTORY,
-    GEOGRAPHY
-} course_type;
+    GEOGRAPHY,
+    TOTAL,
+    RANK,
+    MARK,
+    INVALID_TYPE = -1
+} data_type_enum;
 
-typedef struct {
-    course_type type;
+// typedef struct course_t course_t;
+typedef struct course_t {
     char name[32];
-    int index;
     float score;
+    struct course_t *next;
 } course_t;
 
 typedef struct {
     int rank;
-    int index;
     const char *comment;
 } mark_t;
-
-typedef struct student_t 
-{
-    char name[32];
-    course_t course;
-    int course_count;
-    float total_score;
-    int rank;
-    mark_t mark;
-    student_t *next;
-} student_t;
-// student type struct end
-
-// fruit type struct begin
-typedef enum {
-    APPLE = 1,
-    BANANAS,
-    PEARS,
-    ORANGES,
-    TANGERINES,
-    WATERMELON,
-    STRAWBERRIES
-} fruit_type;
-
-typedef struct fruit_t
-{
-    fruit_type tpye;
-    char * name
-    float costs;
-    float price;
-    float sales_volume;
-    float total;
-    float margins;
-    fruit_t *next;
-} fruit_t;
-// fruit type struct end
-
-
-static info_handle_t g_info_handle_list[] = {
-    {STUDENT, student_info_handler},
-    {FRUIT,   fruit_info_handler}
-};
 
 static mark_t mark_list[] = {
     {1, "Perfect!"},
@@ -104,3 +60,35 @@ static mark_t mark_list[] = {
     {4, "Keep it up!"},
 };
 
+typedef struct student_t
+{
+    char name[32];
+    course_t *course;
+    int course_count;
+    float total_score;
+    int rank;
+    mark_t mark;
+    struct student_t *next;
+} student_t;
+// student type struct end
+
+// fruit type struct begin
+const char *fruit_type_list[] = {"Apple", "Bananas" , "Pears", "Oranges"
+                                 "Tangerines", "Watermelon" , "Strawberries"};
+
+typedef struct fruit_t
+{
+    char * name;
+    float costs;
+    float price;
+    float sales_volume;
+    float total;
+    float margins;
+    struct fruit_t *next;
+} fruit_t;
+// fruit type struct end
+
+typedef struct {
+    int item_count;
+    char **items;
+} character_array_2d_t;
